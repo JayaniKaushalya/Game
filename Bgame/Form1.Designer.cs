e9feae029f66e76e3880931daf625fa920bd79ee
JayaniKaushalya <jayani4775@gmail.com>
2021-05-31 16:16:30 +0530

    initial commit

---
+++ Bgame/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Bgame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.slider = new System.Windows.Forms.PictureBox();
+            this.vert = new System.Windows.Forms.Timer(this.components);
+            this.horz = new System.Windows.Forms.Timer(this.components);
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.slider)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ball
+            // 
+            this.ball.Image = ((System.Drawing.Image)(resources.GetObject("ball.Image")));
+            this.ball.Location = new System.Drawing.Point(12, 12);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(79, 74);
+            this.ball.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ball.TabIndex = 0;
+            this.ball.TabStop = false;
+            // 
+            // slider
+            // 
+            this.slider.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.slider.BackColor = System.Drawing.Color.DodgerBlue;
+            this.slider.Location = new System.Drawing.Point(114, 357);
+            this.slider.Name = "slider";
+            this.slider.Size = new System.Drawing.Size(144, 10);
+            this.slider.TabIndex = 1;
+            this.slider.TabStop = false;
+            // 
+            // vert
+            // 
+            this.vert.Enabled = true;
+            this.vert.Interval = 1;
+            this.vert.Tick += new System.EventHandler(this.vert_Tick);
+            // 
+            // horz
+            // 
+            this.horz.Enabled = true;
+            this.horz.Interval = 1;
+            this.horz.Tick += new System.EventHandler(this.horz_Tick);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(673, 368);
+            this.Controls.Add(this.slider);
+            this.Controls.Add(this.ball);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.slider)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.PictureBox slider;
+        private System.Windows.Forms.Timer vert;
+        private System.Windows.Forms.Timer horz;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Bgame/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bgame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int speed = 3;
+        int vdir = +1;
+        int slide_speed = 10;
+        private void vert_Tick(object sender, EventArgs e)
+        {
+            if (ball.Top < 0)
+            {
+
+                vdir = +1;
+
+            }
+            else if (ball.Top > (this.Height - ball.Height - slider.Height))
+            {
+                vdir = -1;
+
+            }
+            
+
+            
+                ball.Top += (vdir*speed);
+
+            
+
+
+
+        }
+
+        int hdir = +1;
+        bool _left = false, _right = false;
+        private void horz_Tick(object sender, EventArgs e)
+        {
+
+            if (ball.Left < 0)
+            {
+
+                hdir = +1;
+
+            }
+            else if (ball.Left > (this.Width - ball.Width))
+            {
+                hdir = -1;
+
+            }
+           
+
+            
+                ball.Left += (hdir*speed); 
+
+            
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            //active
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (_right && slider.Left<(this.Width=slider.Width))
+            {
+                slider.Left =slider.Left - 10;
+             
+
+            }
+            else if (_right && slider.Left > 0)
+            {
+                slider.Left += 10;
+
+
+            }
+        }
+    }
+}
